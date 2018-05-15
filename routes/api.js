0d8bfb8a660e7f8e8a0fc33e60e19219e6ed431a
Rowan Dorst <rdorst1@avans.nl>
2018-05-15 17:04:27 +0200

    api class updated met correcte errors

--- routes/api.js
+++ routes/api.js
@@ -1,49 +1,93 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jwt-simple')
-const auth = require('../auth/authentication.js')
-const studentenhuis = require('./studentenhuis.js')
+const express = require('express');
+const router = express.Router();
+const jwt = require('jwt-simple');
+const auth = require('../auth/authentication.js');
+const studentenhuis = require('./studentenhuis.js');
 const db = require('../datasource/dbConnection');
+const error = require('../errorsHandler/Errors');
+let regex = require('regex-email');
 
 //
 // Login with {"username":"<username>", "password":"<password>"}
 //
 router.route('/login').post( function(req, res) {
+    let email = req.body.email || '';
+    let password = req.body.password || '';
 
-    //
-    // Get body params or ''
-    //
-    var username = req.body.username || '';
-    var password = req.body.password || '';
+    if(regex.test(email) === true){
+        db.query('SELECT email, password FROM user WHERE email = ?', [email], function (err, rows, fields) {
+            if (err) {
+                res.status(500).json(err)
+                return;
+            }
 
-    //
-    // Check in datasource for user & password combo.
-    //
-    //
-    db.query('SELECT email, password FROM user WHERE email = ?', [username], function (error, rows, fields) {
-        if (error) {
-            res.status(500).json(error)
-        }
+            if(rows.length < 1){
+                error.notFound(res)
+                return;
+            }
 
-        console.log(rows)
-
-        if (username == rows[0].email && password == rows[0].password) {
-            var token = auth.encodeToken(username);
-            res.status(200).json({
-                "token": token,
-                "status": 200,
-                "parameters": res.body
-            });
-        } else {
-            res.status(401).json({
-                "msg": "The credentials you entered are wrong",
-                "status": 401,
-                "parameters": res.body
-            })
-        }
-    })
+            if (email == rows[0].email && password == rows[0].password) {
+                var token = auth.encodeToken(email);
+                res.status(200).json({
+                    "token": 'Bearer ' + token,
+                    "status": 200,
+                    "parameters": res.body
+                });
+            } else {
+                error.noAuth(res)
+                return;
+            }
+        })
+    }else{
+        error.emailIncorrect(res)
+        return;
+    }
 });
 
-router.use('/studentenhuis', studentenhuis);
+
+
+router.route('/register').post( function(req, res){
+    let firstname = req.body.voornaam || '';
+    let lastname = req.body.achternaam || '';
+    let email = req.body.email || '';
+    let password = req.body.password || '';
+
+    if (firstname !== '' && lastname !== '' && email !== '' && password !== '') {
+        if(firstname.length < 2 || lastname.length < 2){
+            error.missingProperties(res)
+            return;
+        }
+
+        if (regex.test(email) === true) {
+            db.query("SELECT Email FROM user WHERE Email = ?", [email], function(err, result) {
+                if(result.length > 0){
+                    error.emailExists(res)
+                    return;
+                }else{
+                    db.query("INSERT INTO `user` (Voornaam, Achternaam, Email, Password) VALUES (?, ?, ?, ?)" ,[firstname, lastname, email, password], function(err, result) {
+                        db.query("SELECT Voornaam, Achternaam, Email FROM user WHERE Email = ?",[email], function(err, result) {
+                            if (err) throw err;
+                        })
+                    })
+                    let token = auth.encodeToken(email)
+                    res.json({
+                        token: token
+                    })
+                    return;
+                }
+            })
+        }
+        else{
+            error.emailIncorrect(res)
+            return;
+        }
+    }
+    else{
+        error.missingProperties(res)
+        return;
+    }
+});
+
+router.use('/studentenhuis', studentenhuis)
 
 module.exports = router;
