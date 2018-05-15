48671d0ef542f18568a634484410c3a795189497
Niek <niek_dooper@hotmail.com>
2018-05-15 18:46:39 +0200

    login connectie met DB, werkt niet

--- routes/Login.js
+++ routes/Login.js
@@ -3,55 +3,38 @@ const express = require('express');
 const router = express.Router();
 const auth =  require('../auth/authentication');
 const users = require('../datasource/logins');
-
-// router.all( new RegExp("[^(\/login)]"), function (req, res, next) {
-//
-//     //
-//     console.log("VALIDATE TOKEN")
-//
-//     var token = (req.header('X-Access-Token')) || '';
-//
-//     auth.decodeToken(token, (err, payload) => {
-//         if (err) {
-//             console.log('Error handler: ' + err.message);
-//             res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
-//         } else {
-//             next();
-//         }
-//     });
-// });
+const db = require('../datasource/dbConnection');
 
 
 router.route('/login')
     .post( function(req, res) {
+        let email = req.body.email || '';
+        let password = req.body.password || '';
 
-        //
-        // Get body params or ''
-        //
-        var email = req.body.email || '';
-        var password = req.body.password || '';
-
-        //
-        // Check in datasource for user & password combo.
-        //
-        //
-        result = users.filter(function (user) {
-            if( user.email === email && user.password === password) {
-                return ( user );
-            }
-        });
-
-        // Debug
-        console.log("result: " +  JSON.stringify(result[0]));
-
-        // Generate JWT
-        if( result[0] ) {
-            res.status(200).json({"token" : auth.encodeToken(email), "email" : email});
+        if (email === '' || password === '') {
+            res.status(412).json({"error": "Niet alle informatie is ingevuld"});
         } else {
-            res.status(401).json({"error":"Invalid credentials, bye"})
+            db.query('SELECT email, password FROM user WHERE email = ?', [email], function (err, rows, fields) {
+                if (err) {
+                    res.status(500).json(err);
+                }
+
+                if (rows.length < 1) {
+                    res.status(404).json(err);
+                }
+
+                if (email === rows[0].email && password === rows[0].password) {
+                    let token = auth.encodeToken(email);
+                    res.status(200).json({
+                        "token": token,
+                        "status": 200,
+                        "parameters": res.body
+                    });
+                } else {
+                    res.status(401).json(err);
+                }
+            })
         }
-
     });
-
 
 module.exports = router;
